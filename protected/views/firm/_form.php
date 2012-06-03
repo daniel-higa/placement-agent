c077e610450de22f8a59ccf184d7c0e1ec59b9e4
Daniel Higa <daniel-higa@bugme-not.com>
2012-06-03 16:17:55 -0300

    +add missing sync continent/region on firm form

--- protected/views/firm/_form.php
+++ protected/views/firm/_form.php
@@ -29,20 +29,42 @@
 		<?php echo $form->dropDownList($model, 'firmtype_id', GxHtml::listDataEx(Firmtype::model()->findAllAttributes(null, true))); ?>
 		<?php echo $form->error($model,'firmtype_id'); ?>
 		</div><!-- row -->
+        
+		<script type="text/javascript">
+            function continentClick(c){
+                $('input[name=firmregions\\[\\]]').each(function(i,e) {
+                   if ($(e).attr('data') == $(c).val()) {
+                      $(e).attr('checked', c.checked);
+                   }
+                }
+                );
+            }
+		</script>
 		
-		<div>
+		<div class="left append-1">
 			<label>Continents</label>
 			<div id="divcontainers">
-			<?php echo CHtml::checkBoxList("firmcontinents", CHtml::listData($model->firmcontinents,'continent_id','continent_id'), CHtml::listData(Continent::model()->findAll(),'id','name'),array('separator'=>'', 'template'=>'<span>{input} {label}</span>')); ?>
+			<?php echo CHtml::checkBoxList("firmcontinents", CHtml::listData($model->firmcontinents,'continent_id','continent_id'), CHtml::listData(Continent::model()->findAll(),'id','name'),array('separator'=>'', 'template'=>'<span>{input} {label}</span>', 'onclick' => 'continentClick(this)')); ?>
 			</div>
 		</div>
 		
-		<div style="display: block; margin-top: -50px; margin-left:200px;">
+		<div class="left">
 			<label>Regions</label>
 			<div id="divcontainers">
-			<?php echo CHtml::checkBoxList("firmregions", CHtml::listData($model->firmregions,'region_id','region_id'), CHtml::listData(Region::model()->findAll(),'id','name'),array('separator'=>'', 'template'=>'<span>{input} {label}</span>')); ?>
-			</div>
+			<?php //echo CHtml::checkBoxList("firmregions", CHtml::listData($model->firmregions,'region_id','region_id'), CHtml::listData(Region::model()->findAll(),'id','name'),array('separator'=>'', 'template'=>'<span>{input} {label}</span>')); ?>
+			<?php //echo
+                //CHtml::checkBoxList("lpregions", CHtml::listData($model->lpregions,'region_id','region_id'), CHtml::listData(Region::model()->findAll(),'id','name'),array('separator'=>'', 'template'=>'<span>{input} {label} </span>'));
+                $firmr = CHtml::listData($model->firmregions,'region_id','region_id');
+                foreach(Region::model()->findAll() as $r) {
+                    echo '<span>';
+                    echo CHtml::checkBox("firmregions[]", in_array($r->id, $firmr), array('value' => $r->id, 'data' => $r->continent_id));
+                    echo CHtml::label($r->name, false);
+                    echo '</span>';
+                }
+            ?>
+            </div>
 		</div>
+        <div class="clearfix"> </div>
 		
 		<div class="row">
 		<?php echo $form->labelEx($model,'rank'); ?>
